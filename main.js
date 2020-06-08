411fc78f3e95a0c089ba2c38be1644997d6e8874
Duy Nguyen <dtn9197@gmail.com>
2020-06-08 13:43:40 -0400

    commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+
+function createWindow() {
+    var mainWindow = new BrowserWindow();
+
+    mainWindow.loadFile(path.join(__dirname, "mainWindow.html"));
+
+     //Quit app when closed
+     mainWindow.on("closed", function() {
+        app.quit();
+    })
+}
+
+app.on("ready", function() {
+    createWindow();
+})
