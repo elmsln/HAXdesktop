a2521456db11d9fd95e059bd47d188334b0444d2
Duy <dtn9197@gmail.com>
2020-06-15 15:54:02 -0400

    commit

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const {app, BrowserWindow} = require("electron");
 const path = require("path");
+//auto update, need to be worked on
+//need to set a flag that this api understands
 require("update-electron-app")();
+
+//flag for production or dev environment
+process.env.NODE_ENV = "development";
+
+
+
+
 
 function createWindow() {
     var mainWindow = new BrowserWindow();
 
     mainWindow.loadFile(path.join(__dirname, "mainWindow.html"));
+    if(process.env.NODE_ENV === "production")
+        mainWindow.removeMenu();
 
      //Quit app when closed
      mainWindow.on("closed", function() {
