95e58a47d5638c0df7817925db6e0defb0c90ae0
Duy <dtn9197@gmail.com>
2020-06-15 23:13:30 -0400

    hax editor enabled

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, Menu} = require("electron");
 const path = require("path");
+
 //auto update, need to be worked on
 //need to set a flag that this api understands
 require("update-electron-app")();
@@ -7,12 +8,27 @@ require("update-electron-app")();
 //flag for production or dev environment
 process.env.NODE_ENV = "development";
 
-
+//create menu with chrome devtool
+var mainMenuTemplate = [
+    {
+        label: 'Developer Tools',
+        click(item, focusedWindow) {
+            focusedWindow.toggleDevTools();
+        }
+        
+    }
+];
 
 
 
 function createWindow() {
+
     var mainWindow = new BrowserWindow();
+
+    //add dev tool option to menu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
 
     mainWindow.loadFile(path.join(__dirname, "mainWindow.html"));
     if(process.env.NODE_ENV === "production")
@@ -22,8 +38,12 @@ function createWindow() {
      mainWindow.on("closed", function() {
         app.quit();
     })
+
+    
+     
 }
 
 app.on("ready", function() {
     createWindow();
 })
+
